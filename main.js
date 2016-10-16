7bda6642d6e6cf84f3ceb8c18c16fc34f2e63bd8
jacksonmerg <jacksonmerg@gmail.com>
2016-10-16 13:58:29 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 var pets = 0;
-$("#sell").hide()
-$("#moneycount").hide()
+$("#sell").hide();
+$("#moneycount").hide();
 var onpet = function(){
 pets++;
 document.getElementById("count").innerHTML = "You have pet eddie " + String(pets)+" time(s)";
 if(pets>29){
-$("#sell").show()
-$("#moneycount").show()
+$("#sell").show();
+$("#moneycount").show();
 }
 };
