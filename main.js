aadcc2a4a1cfd7954a835d16679942623e3eaa7f
jacksonmerg <jacksonmerg@gmail.com>
2016-10-16 15:33:32 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 var pets = 0;
 var money = 0;
+var mps = 0;
+var clones = 0;
 $("#sell").hide();
 $("#moneycount").hide();
 var onpet = function(){
@@ -9,8 +11,35 @@ if(pets>29){
 $("#sell").show();
 $("#moneycount").show();
 }
+if(pets>99){
+$("#clone").show();
+$("#clonecount").show();
+}
 };
 var sell = function(){
 money++;
+if(mps == 0){
 document.getElementById("moneycount").innerHTML = "You have " + String(money)+"$";
-}
+}else{
+document.getElementById("moneycount").innerHTML = "You have " + String(money)+"$ ("+mps+"/s)";
+}};
+var clone = function(){
+clones++;
+document.getElementById("clonecount").innerHTML = "You have " + String(pets)+" Eddie clones";
+};
+var buildbooth = function(){
+if(money>19){
+  clones--;
+  money-=20;
+  mps++;
+  document.getElementById("clonecount").innerHTML = "You have " + String(pets)+" Eddie clones";
+  document.getElementById("moneycount").innerHTML = "You have " + String(money)+"$ ("+mps+"/s)";
+}};
+var onTick = function(){
+  money+=mps
+  if(mps == 0){
+document.getElementById("moneycount").innerHTML = "You have " + String(money)+"$";
+}else{
+document.getElementById("moneycount").innerHTML = "You have " + String(money)+"$ ("+mps+"/s)";
+}}
+setInterval("onTick()", 3000);
